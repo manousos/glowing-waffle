e71c903538717cb841c1dd299a1b394447cbb872
Thefolle <cdavide8@gmail.com>
2022-07-23 15:06:19 +0200

    Learn permission architecture

--- CandidateRules.md
+++ CandidateRules.md
@@ -3,3 +3,5 @@
 - Check that all dependencies declared in the pom correspond to the latest in the remote repositories (https://stackoverflow.com/questions/10362540/how-to-check-the-lastest-dependency-on-maven);
 - Fail the build when a directory contains an unwanted character (https://stackoverflow.com/questions/42341897/how-can-i-check-if-a-filename-in-my-maven-project-contains-certain-characters-a);
 - Check a property no matters if it comes from environment or it is defined by the user (https://issues.apache.org/jira/projects/MENFORCER/issues/MENFORCER-399?filter=allopenissues)
+- Specify parameters to rules from CLI: https://stackoverflow.com/questions/60265976/add-parameters-to-maven-enforcer-rules-called-from-the-command-line;
+- Check elevated privileges: https://stackoverflow.com/questions/45423340/does-a-maven-plugin-exist-to-gracefully-check-elevated-privilege.
---
+++ src/main/java/org/apache/maven/enforcer/rule/requirePrivilegedPermission.java
@@ -0,0 +1,104 @@
+package org.apache.maven.enforcer.rule;
+
+import java.io.File;
+import java.io.FilePermission;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.List;
+
+import org.apache.maven.enforcer.rule.api.EnforcerLevel;
+import org.apache.maven.enforcer.rule.api.EnforcerRule;
+import org.apache.maven.enforcer.rule.api.EnforcerRule2;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
+
+public class requirePrivilegedPermission implements EnforcerRule2 {
+
+    private EnforcerLevel level = EnforcerLevel.ERROR;
+
+    @Override
+    public void execute(EnforcerRuleHelper helper) throws EnforcerRuleException {
+        // ThreadUtils.getAllThreads().stream().forEach(thread -> {
+        //     helper.getLog().error(thread.getId() + ": " + thread.getPriority());
+        // });
+
+        /**
+         *                  | Admin console | user console
+         * is daemon            x               x
+         * priority             5               5
+         * missing perms        modifyThread, closeClassLoader, getClassLoader    setIO, modifyThread, closeClassLoader, getClassLoader
+         */
+
+        // AllPermission allPermission = new AllPermission();
+        // helper.getLog().error(allPermission.getActions());
+
+        // SecurityManager securityManager = new SecurityManager();
+        
+        // helper.getLog().error(Thread.currentThread().getPriority() + "");
+        // System.setSecurityManager(securityManager);
+        // RuntimePermission runtimePermission = new RuntimePermission("setIO");
+        // helper.getLog().error(runtimePermission.getActions());
+        // AccessController.checkPermission(new RuntimePermission(""));
+        // AccessController.doPrivileged(new PrivilegedAction<Integer>() {
+        //     @Override
+        //     public Integer run() {
+        //         return 3;
+        //     }
+        // });
+
+        File file = new File("pom.xml");
+        helper.getLog().error(file.exists() + "");
+        helper.getLog().error(file.canWrite() + "");
+        // AccessController.checkPermission(new RuntimePermission("setIO"));
+        Thread[] threadArray = new Thread[20];
+        Thread.currentThread().getThreadGroup().enumerate(threadArray, true);
+        List<Thread> threads = List.of(threadArray);
+        threads.forEach(thread -> helper.getLog().error(thread.toString()));
+        
+        
+        // AccessController.doPrivileged(new PrivilegedAction<Integer>() {
+        //     @Override
+        //     public Integer run() {
+        //         // helper.getLog().error(new FilePermission("C:\\Users\\davide.calarco\\Desktop\\snippets\\Maven\\test-plugin\\demo\\pom.xml"));
+
+        //         helper.getLog().error(file.canExecute() + "");
+        //         return 0;
+        //     } 
+        // });
+        // AccessController.checkPermission(new FilePermission("C:\\Users\\davide.calarco\\Desktop\\snippets\\Maven\\test-plugin\\demo\\pom.xml", "read"));
+        
+        // helper.getLog().error(System.getProperty("user.name"));
+        // helper.getLog().error(System.getProperties().toString());
+        // securityManager.checkPermission(new RuntimePermission("setIO"));
+        // securityManager.checkPermission();
+
+        // whoami -> regroup\davide.calarco
+        
+    }
+
+    @Override
+    public boolean isCacheable() {
+        return false;
+    }
+
+    @Override
+    public boolean isResultValid(EnforcerRule cachedRule) {
+        return false;
+    }
+
+    @Override
+    public String getCacheId() {
+        return null;
+    }
+
+    @Override
+    public EnforcerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = EnforcerLevel.valueOf(level.toUpperCase()); // should throw exception if the string is not
+                                                                 // recognized
+    }
+    
+}
